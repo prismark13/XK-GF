1d40729885aa15d26ba0ae1ad1ad5428b3938afa
prismark13 <109438232+prismark13@users.noreply.github.com>
2026-01-17 09:32:17 -0500

    Update 6_club_opening.js

--- js/sections/6_club_opening.js
+++ js/sections/6_club_opening.js
@@ -1,54 +1,107 @@
 export const clubOpening = `
 <details>
-    <summary><span>Strong 2<span class="suit-c">♣</span></span></summary>
+    <summary><span>2<span class="suit-b">♣</span> Strong Opening</span></summary>
     <div class="content">
-        <div class="subheader-bar">Responses</div>
+        
+        <div class="subheader-bar">Responses to 2<span class="suit-b">♣</span></div>
         <div class="table-wrapper">
             <table>
                 <thead>
-                    <tr><th class="col-resp">Resp</th><th class="col-meaning">Meaning</th><th class="col-cont">Notes</th></tr>
+                    <tr>
+                        <th style="width: 15%">Bid</th>
+                        <th style="width: 20%">Meaning</th>
+                        <th>Notes</th>
+                    </tr>
                 </thead>
                 <tbody>
                     <tr>
-                        <td class="col-resp"><span class="suit-d">2♦</span></td>
+                        <td><span class="suit-r">2♦</span></td>
                         <td><strong>Waiting</strong></td>
-                        <td>Standard Waiting (includes negatives).</td>
+                        <td>Artificial. Majority of hands (0+ HCP).</td>
                     </tr>
                     <tr>
-                        <td class="col-resp"><span class="suit-h">2♥</span></td>
-                        <td><strong>Double Neg</strong></td>
-                        <td>Immediate negative (Bust).</td>
+                        <td>2<span class="suit-r">♥</span>/2<span class="suit-b">♠</span><br>3<span class="suit-b">♣</span>/3<span class="suit-r">♦</span></td>
+                        <td><strong>Positive</strong></td>
+                        <td>8+ HCP. Good 5+ suit (2 of top 3 honors).</td>
                     </tr>
                     <tr>
-                        <td class="col-resp">2<span class="suit-s">♠</span></td>
-                        <td><strong>5+ Hearts</strong></td>
-                        <td>Positive Transfer to Hearts (GF).</td>
-                    </tr>
-                    <tr>
-                        <td class="col-resp">2NT</td>
-                        <td><strong>5+ Spades</strong></td>
-                        <td>Positive Transfer to Spades (GF).</td>
+                        <td>2NT</td>
+                        <td><strong>Balanced</strong></td>
+                        <td>8-10 HCP. Rare (prefer 2<span class="suit-r">♦</span>).</td>
                     </tr>
                 </tbody>
             </table>
         </div>
 
-        <div class="subheader-bar">Kokish Relay (After 2♦)</div>
+        <div class="subheader-bar">Opener's Rebids (after 2<span class="suit-b">♣</span> - 2<span class="suit-r">♦</span>)</div>
         <div class="table-wrapper">
             <table>
                 <tbody>
-                    <tr>
-                        <td class="col-resp">2<span class="suit-h">♥</span></td>
-                        <td><strong>Kokish</strong></td>
+                    <tr style="border-bottom: 2px solid #eee;">
+                        <td style="width: 15%"><strong>2NT</strong></td>
+                        <td style="width: 20%"><strong>22-24 Bal</strong></td>
                         <td>
-                            <strong>Either</strong> Hearts (Natural) <strong>OR</strong> 25+ Balanced.<br>
-                            Responder forces <strong>2<span class="suit-s">♠</span></strong>.
+                            <strong>System ON</strong> (4-Way Transfers):
+                            <div style="margin-top: 4px; font-size: 0.9em;">
+                                3<span class="suit-b">♣</span>&rarr;<span class="suit-r">♥</span> &nbsp;|&nbsp; 
+                                3<span class="suit-r">♦</span>&rarr;<span class="suit-b">♠</span> &nbsp;|&nbsp; 
+                                3<span class="suit-r">♥</span>&rarr;<span class="suit-b">♣</span> &nbsp;|&nbsp; 
+                                3<span class="suit-b">♠</span>&rarr;<span class="suit-r">♦</span>
+                            </div>
+                        </td>
+                    </tr>
+
+                    <tr style="border-bottom: 2px solid #eee;">
+                        <td><span class="suit-r"><strong>2♥</strong></span></td>
+                        <td><strong>Kokish Relay</strong></td>
+                        <td>
+                            Shows <span class="suit-r">♥</span> <strong>OR</strong> 25+ Balanced.<br>
+                            <strong>Resp 2<span class="suit-b">♠</span> forced.</strong> Opener rebids:
+                            <table class="nested-table" style="margin-top:5px;">
+                                <tr>
+                                    <td class="nested-key">2NT</td>
+                                    <td class="nested-val"><strong>25+ Bal.</strong> (Transfers ON).</td>
+                                </tr>
+                                <tr>
+                                    <td class="nested-key">3<span class="suit-r">♥</span></td>
+                                    <td class="nested-val">
+                                        <strong>Natural <span class="suit-r">♥</span> (6+). GF.</strong>
+                                        <div style="font-size: 0.85em; margin-top:2px; color:#555;">
+                                            3<span class="suit-b">♠</span> (Nat), 3NT (Non-Serious), 4m (Cue), 4<span class="suit-r">♥</span> (Fast Arrival).
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class="nested-key">3m</td>
+                                    <td class="nested-val">Natural <span class="suit-r">♥</span> + Minor (5-4). GF.</td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>2<span class="suit-b">♠</span></td>
+                        <td><strong>Natural</strong></td>
+                        <td>
+                            5+ Spades. GF.<br>
+                            <em style="font-size:0.9em">3<span class="suit-b">♣</span> is 2nd Negative (0-3 HCP).</em>
                         </td>
                     </tr>
                     <tr>
-                        <td class="col-resp">2NT</td>
-                        <td><strong>22-24 Bal</strong></td>
-                        <td>Standard 2NT Systems apply.</td>
+                        <td>3<span class="suit-b">♣</span></td>
+                        <td><strong>Natural</strong></td>
+                        <td>
+                            5+ Clubs. GF.<br>
+                            <em style="font-size:0.9em">3<span class="suit-r">♦</span> is 2nd Negative (0-3 HCP).</em>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>3<span class="suit-r">♦</span></td>
+                        <td><strong>Natural</strong></td>
+                        <td>
+                            5+ Diamonds. GF.<br>
+                            <em style="font-size:0.9em">3<span class="suit-r">♥</span> is 2nd Negative (0-3 HCP).</em>
+                        </td>
                     </tr>
                 </tbody>
             </table>
